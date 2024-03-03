a710c17a12153bbe61750f5f9b9ae2752fd1d0f7
GLITCH-08 <virajraina2003@gmail.com>
2024-03-03 19:05:12 +0530

    update

--- Detector.py
+++ Detector.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from tensorflow.python.keras.utils.data_utils import get_file
 
-np.random.seed(123)
+np.random.seed(20)
 
 class Detector:
     def __init__(self):
@@ -28,7 +28,109 @@ class Detector:
         get_file(fname=filename, origin=modelURL, cache_dir=self.cacheDir , cache_subdir="checkpoints" , extract=True)
 
 
-    def loadModel(self):
+    def loadModel(self):  
         print("Loading Model "+ self.modelName)
         tf.keras.backend.clear_session()
-        self.model = tf.saved_model.load(os.path.join(self.cacheDir, "checkpoint" , self.modelName))
+        self.model = tf.saved_model.load(os.path.join(self.cacheDir, "checkpoints" , self.modelName , "saved_model"))
+
+        print("Model " + self.modelName + "loaded sucess!!")
+
+
+    def createBoundingBox(self,image, threshold= 0.5):
+        inputTensor = cv2.cvtColor(image.copy(), cv2.COLOR_BGR2RGB)
+        inputTensor = tf.convert_to_tensor(inputTensor, dtype=tf.uint8)
+        inputTensor = inputTensor[tf.newaxis,...]
+
+        detections = self.model(inputTensor)
+
+        bboxs = detections['detection_boxes'][0].numpy()
+        classIndexes = detections['detection_classes'][0].numpy().astype(np.int32)
+        classScores = detections['detection_scores'][0].numpy()
+
+
+        imH, imW, imC = image.shape
+
+        bboxIdx = tf.image.non_max_suppression(bboxs, classScores, max_output_size=50, iou_threshold=0.5, score_threshold=0.5)        
+
+        print(bboxIdx)
+
+        if len(bboxIdx) != 0:
+            for i in bboxIdx:
+                bbox = tuple(bboxs[i].tolist())
+                classConfidence = round(100*classScores[i])
+                classIndex = classIndexes[i]
+
+                classLabelText = self.classesList[classIndex].upper()
+                classColor = self.colorList[classIndex]
+
+
+                displayText = '{}: {}%'.format(classLabelText, classConfidence)
+
+                ymin, xmin, ymax, xmax = bbox
+
+                xmin, xmax ,ymin ,ymax = (xmin*imW, xmax* imW ,ymin*imH ,ymax*imH)
+                xmin, xmax ,ymin ,ymax = int(xmin), int(xmax) ,int(ymin) ,int(ymax)
+
+
+                cv2.rectangle(image,(xmin,ymin),(xmax,ymax),color=classColor,thickness=1)
+                cv2.putText(image,displayText,(xmin,ymin - 10),cv2.FONT_HERSHEY_PLAIN,1, classColor,2)
+
+                lineWidth = min(int((xmax - xmin)*0.2), int((ymax - ymin)*0.2))
+
+                cv2.line(image,(xmin,ymin), (xmin + lineWidth,ymin),classColor,thickness=5)
+                cv2.line(image,(xmin,ymin), (xmin,ymin + lineWidth),classColor,thickness=5)
+
+                cv2.line(image,(xmax,ymin), (xmax- lineWidth,ymin),classColor,thickness=5)
+                cv2.line(image,(xmax,ymin), (xmax,ymin + lineWidth),classColor,thickness=5)
+
+                cv2.line(image,(xmin,ymax), (xmin + lineWidth,ymax),classColor,thickness=5)
+                cv2.line(image,(xmin,ymax), (xmin,ymax - lineWidth),classColor,thickness=5)
+
+                cv2.line(image,(xmax,ymax), (xmax- lineWidth,ymax),classColor,thickness=5)
+                cv2.line(image,(xmax,ymax), (xmax,ymax - lineWidth),classColor,thickness=5)
+
+        return image
+
+
+
+
+    def predictImage(self, imagePath, threshold=0.5):
+        image = cv2.imread(imagePath)
+
+        bboxImage = self.createBoundingBox(image)
+
+
+        cv2.imwrite(self.modelName + ".jpg", bboxImage)
+        cv2.imshow("Result", bboxImage)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def predictVideo(self, videoPath, threshold = 0.5):
+        cap = cv2.VideoCapture(videoPath)
+
+        if(cap.isOpened()== False):
+            print("Error while openning")
+            return
+        
+        (success, image) = cap.read()
+
+        startTime = 0
+
+        while True:
+            currentTime = time.time()
+
+            fps = 1/(currentTime - startTime)
+            startTime = currentTime
+
+            bboxImage = self.createBoundingBox(image, threshold)
+
+            cv2.putText(bboxImage, "FPS: " + str(int(fps)), (20,70 ), cv2.FONT_HERSHEY_PLAIN,2 ,(0,255,0),2)
+            cv2.imshow("result",bboxImage)
+
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord("q"):
+                break
+
+            (success, image) = cap.read()
+
+        cv2.destroyAllWindows()
--- run.py
+++ run.py
@@ -1,8 +1,15 @@
 from Detector import *
 
-modelURL = "http://download.tensorflow.org/models/object_detection/tf2/20200711/faster_rcnn_resnet50_v1_640x640_coco17_tpu-8.tar.gz"
+modelURL = "http://download.tensorflow.org/models/object_detection/tf2/20200711/faster_rcnn_inception_resnet_v2_640x640_coco17_tpu-8.tar.gz"
 
 classFile = "coco.names"
+imagePath = "testing/2.jpg"
+videoPath = "testing/walk.mp4"
+threshold=0.5
+
 detector= Detector()
 detector.readClasses(classFile)
 detector.downloadModel(modelURL)
+detector.loadModel()
+# detector.predictImage(imagePath,threshold)
+detector.predictVideo(videoPath,threshold)
